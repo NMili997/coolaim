16d5c22f8f1788770e93640bee74ed201c24ac06
NMili997 <milivojevic97@outlook.com>
2020-04-01 19:01:41 +0200

    Updates

---
+++ precache-manifest.d9f950afba74baaa984ca4f242dfe7d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd552b9dedaf1c4ea9169108cc2d482",
+    "url": "/coolaim/index.html"
+  },
+  {
+    "revision": "21871a6903fc6b71938c",
+    "url": "/coolaim/static/css/main.8cef392d.chunk.css"
+  },
+  {
+    "revision": "a64513096f80e1350490",
+    "url": "/coolaim/static/js/2.4af48f9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coolaim/static/js/2.4af48f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21871a6903fc6b71938c",
+    "url": "/coolaim/static/js/main.622d1ee6.chunk.js"
+  },
+  {
+    "revision": "5c93480204cecdb57a52",
+    "url": "/coolaim/static/js/runtime-main.30662885.js"
+  },
+  {
+    "revision": "850027152f34479300dedb6aacc6fc10",
+    "url": "/coolaim/static/media/login.85002715.svg"
+  }
+]);
